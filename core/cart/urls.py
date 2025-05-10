be3cbe4f0582de94608411130d45d404311d776f
Sadegh Mehdizadeh <sdg.mhz@gmail.com>
2025-05-10 20:13:52 +0330

    add SessionAddProduct view and complete add to cart functionality

--- core/cart/urls.py
+++ core/cart/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from . import views
+
 app_name = "cart"
 
-urlpatterns = []
+urlpatterns = [
+    path("session/add-product/", views.SessionAddProduct.as_view(), name="session-add-product"),
+]
--- core/cart/views.py
+++ core/cart/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import View
+from django.http import JsonResponse
 
-# Create your views here.
+from .cart import CartSession
+
+
+class SessionAddProduct(View):
+    
+    def post(self, request, *args, **kwargs):
+        cart = CartSession(request.session)
+        product_id = request.POST.get("product_id")
+        if product_id:
+            cart.add_product(product_id)
+        return JsonResponse({"cart": cart.get_cart_dict()})
