ab5c89c17ce7dc64f48025a41340e4e6b146521b
Sadegh Mehdizadeh <sdg.mhz@gmail.com>
2025-05-19 22:12:21 +0330

    add security-edit page for admin dashboard

---
+++ core/dashboard/admin/forms.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import forms as auth_forms
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+
+class AdminPasswordChangeForm(auth_forms.PasswordChangeForm):
+
+    error_messages = {
+        "password_incorrect": _(
+            "پسورد فعلی شما اشتباه وارد شده است. لطفا تصحیح نمایید."
+        ),
+        "password_mismatch": _("دو پسورد وردی با همدیگر مطابقت ندارند"),
+    }
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(user, *args, **kwargs)
+        self.fields["old_password"].widget.attrs["class"] = "form-control text-center"
+        self.fields["new_password1"].widget.attrs["class"] = "form-control text-center"
+        self.fields["new_password2"].widget.attrs["class"] = "form-control text-center"
+        self.fields["old_password"].widget.attrs[
+            "placeholder"
+        ] = "پسورد فعلی را وارد نمایید"
+        self.fields["new_password1"].widget.attrs[
+            "placeholder"
+        ] = "پسورد جدید را وارد نمایید"
+        self.fields["new_password2"].widget.attrs[
+            "placeholder"
+        ] = "مجددا پسورد جدید را وارد نمایید"
--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -1,10 +1,26 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
 from ..permissions import HasAdminAccessPermission
+from .forms import AdminPasswordChangeForm
 
 
 class AdminDashboardHomeView(
     LoginRequiredMixin, HasAdminAccessPermission, TemplateView
 ):
     template_name = "dashboard/admin/home.html"
+
+
+class AdminSecurityEditView(
+    LoginRequiredMixin,
+    HasAdminAccessPermission,
+    SuccessMessageMixin,
+    auth_views.PasswordChangeView,
+):
+    template_name = "dashboard/admin/profile/security-edit.html"
+    form_class = AdminPasswordChangeForm
+    success_url = reverse_lazy("dashboard:admin:security-edit")
+    success_message = "بروزرسانی پسورد با موفقیت انجام شد"
