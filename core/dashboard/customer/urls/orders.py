a86baa6828471ac10944ec4fdab565c110056217
Sadegh Mehdizadeh <sdg.mhz@gmail.com>
2025-06-27 14:56:22 +0330

    add orders to customer dashboard

---
+++ core/dashboard/customer/urls/orders.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .. import views
+
+
+urlpatterns = [
+    path("order/list/", views.CustomerOrderListView.as_view(), name="order-list"),
+    path("order/<int:pk>/detail/", views.CustomerOrderDetailView.as_view(), name="order-detail"),
+
+]
