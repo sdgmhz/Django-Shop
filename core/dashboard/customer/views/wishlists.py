cbfd2fde2cee43e4a0d4c50e2313b78f16549b22
Sadegh Mehdizadeh <sdg.mhz@gmail.com>
2025-07-05 12:14:32 +0330

    add wishlist feature for customers

---
+++ core/dashboard/customer/views/wishlists.py
@@ -0,0 +1,36 @@
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import FieldError
+
+
+from shop.models import WishlistProductModel
+from dashboard.permissions import HasCustomerAccessPermission
+
+
+class CustomerWishlistListView(
+    LoginRequiredMixin, HasCustomerAccessPermission, ListView
+):
+    template_name = "dashboard/customer/wishlists/wishlist-list.html"
+
+    paginate_by = 2
+
+    def get_paginate_by(self, queryset):
+        """Return the number of products to display per page (based on query parameter)"""
+        return self.request.GET.get("page_size", self.paginate_by)
+
+    def get_queryset(self):
+        queryset = WishlistProductModel.objects.filter(user=self.request.user)
+        if search_q := self.request.GET.get("q"):
+            queryset = queryset.filter(product__title__icontains=search_q)
+        if order_by := self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        """Add additional context (total item count ) to the context"""
+        context = super().get_context_data(**kwargs)
+        context["total_items"] = self.get_queryset().count()  
+        return context
--- core/shop/admin.py
+++ core/shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ProductModel, ProductImageModel, ProductCategoryModel
+from .models import ProductModel, ProductImageModel, ProductCategoryModel, WishlistProductModel
 
 
 @admin.register(ProductModel)
@@ -33,3 +33,10 @@ class ProductImageModel(admin.ModelAdmin):
     """Admin configuration for ProductImageModel"""
 
     list_display = ("id", "file", "created_date")
+
+
+@admin.register(WishlistProductModel)
+class WishlistProductModelAdmin(admin.ModelAdmin):
+    """Admin configuration for WishlistProductModel"""
+
+    list_display = ("id", "user", "product")
